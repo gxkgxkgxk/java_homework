61e3b2b57eb2bdf560ad6cbdbb2fb3225d072a37
Ricardo <dangguigxk@outlook.com>
2018-11-18 16:00:50 +0800

    修改

--- src/main/java/gxk/java/homework/HomeworkApplication.java
+++ src/main/java/gxk/java/homework/HomeworkApplication.java
@@ -2,6 +2,7 @@ package gxk.java.homework;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @SpringBootApplication
 public class HomeworkApplication {
---
+++ src/main/java/gxk/java/homework/POJO/User.java
@@ -0,0 +1,15 @@
+package gxk.java.homework.POJO;
+
+import lombok.Data;
+
+/**
+ * @Author 郭效坤
+ * @Date 2018/11/18 15:41
+ * @Description
+ */
+@Data
+public class User {
+
+    private String username;
+    private String password;
+}
