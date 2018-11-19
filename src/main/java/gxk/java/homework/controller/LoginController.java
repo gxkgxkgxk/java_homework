9b34001264c0618057e8d1348140b35fb059b294
Ricardo <dangguigxk@outlook.com>
2018-11-19 13:15:33 +0800

    配置nginx

--- src/main/java/gxk/java/homework/controller/LoginController.java
+++ src/main/java/gxk/java/homework/controller/LoginController.java
@@ -1,21 +1,21 @@
 package gxk.java.homework.controller;
 
 import gxk.java.homework.POJO.User;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 /**
  * @Author 郭效坤
  * @Date 2018/11/18 15:17
  * @Description
  */
-@RestController
-@RequestMapping("/login")
+@Controller
+@RequestMapping("/homework")
 public class LoginController {
 
-    @PostMapping("/test")
+    @PostMapping("/login/test")
     public String loginTest(@RequestBody User user){
         System.out.println(user.toString());
         return "management";
