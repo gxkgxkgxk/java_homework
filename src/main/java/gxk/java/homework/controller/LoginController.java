af038aee7a9d410df171876e3df38b7a23250883
Ricardo <dangguigxk@outlook.com>
2018-11-20 14:25:09 +0800

    配置nginx,初步实现跳转

--- src/main/java/gxk/java/homework/controller/LoginController.java
+++ src/main/java/gxk/java/homework/controller/LoginController.java
@@ -2,22 +2,19 @@ package gxk.java.homework.controller;
 
 import gxk.java.homework.POJO.User;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * @Author 郭效坤
  * @Date 2018/11/18 15:17
  * @Description
  */
-@Controller
-@RequestMapping("/homework")
+@RestController
+@RequestMapping("/login")
 public class LoginController {
-
-    @PostMapping("/login/test")
-    public String loginTest(@RequestBody User user){
+    @PostMapping("/test/crazy")
+    public User loginTest(@RequestBody User user){
         System.out.println(user.toString());
-        return "management";
+        return user;
     }
 }
